df4b41b6392422e192995e79945c99148273ce0a
Amadeo Bon <amadeosph@outlook.com>
2023-04-02 03:03:45 -0300

    typescript

---
+++ src/modulo-26.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(num1, num2) {
+    const resulted = num1 * num2;
+    console.log(`O resultado da multiplicação entre ${num1} e ${num2} é ${resulted}`);
+    return resulted;
+}
+function saudar(nome) {
+    const mensagem = `Olá ${nome}!`;
+    console.log(mensagem);
+    return mensagem;
+}
---
+++ src/modulo-26.ts
@@ -0,0 +1,12 @@
+function multiplicar(num1: number, num2: number): number {
+    const resulted = num1 * num2;
+    console.log(`O resultado da multiplicação entre ${num1} e ${num2} é ${resulted}`);
+    return resulted;
+   
+  }
+  
+  function saudar(nome: string): string {
+    const mensagem = `Olá ${nome}!`;
+    console.log(mensagem);
+    return mensagem;
+  }
